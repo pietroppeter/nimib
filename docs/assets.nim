593b79e11a6abad47d3cd94183b588cdab801ffe
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-02-18 13:57:43 +0100

    fix tripleQuote in assets

--- docs/assets.nim
+++ docs/assets.nim
@@ -3,11 +3,12 @@ import nimib
 nbInit
 nbText: """# Assets"""
 nbCode:
-  import strformat
+  import strformat, strutils
 nbText: """
 List of assets that should be available in order to be able to create
 a working html document starting from a single nim file and nothing else:
 """
+# note that ideally highlighting of nim could be done avoiding js and during rendering!
 nbCode:
   let assetFiles: seq[(string, RelativeFile)] = @[
       ("highlightJs", "./static/highlight.nim.js".RelativeFile),
@@ -21,25 +22,29 @@ the `nimib\assets.nim` module reading from this files.
 """
 nbCode:
   let assetModuleFile = "../src/nimib/assets.nim".RelativeFile
-  let tripleQuote = "\"\"\""
-  var assetContent: string
+  let
+    tripleQuote = "\"\"\""
+    tripleQuoteReplacement = tripleQuote & &" & \"\\\"\\\"\\\"\" & " & tripleQuote
+  var assetModuleContent: string
   for (name, file) in assetFiles:
-    assetContent.add fmt"""
+    let content = file.read.replace(tripleQuote, tripleQuoteReplacement)
+    assetModuleContent.add fmt"""
 let {name}* = {tripleQuote}
-{file.read}
-{tripleQuote}
+{content}{tripleQuote}
 """
-  assetModuleFile.write assetContent
+  assetModuleFile.write assetModuleContent
 
+# add test for asset.nim that checks content is same as original file
 nbSave
 #[
   changes:
-    - NbDoc type will have fields templateDirs and partials:
+    - [X] NbDoc type will have fields templateDirs and partials:
       they will be added to Context during nbSave.
       In this way partial can be changed and overwritten.
-    - if highlight files are found as static they are used as static,
+    - [ ] if highlight files are found as static they are used as static,
       otherwise their content is copied in the html.
-    - templates instead are just copied in memory in the partials
+    - [ ] templates instead are just copied in memory in the partials
       (no need to use them as static). They should be written to file
-      only in case you need to customize them for multiple files.
+      only in case you need to customize them for multiple files
+      (in that case remember to drop them from partial table).
 ]#

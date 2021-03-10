af13a703355904947c4ad1f8f58e2c3022e80b33
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-03-10 15:58:30 +0100

    nolan: better explanation

--- docs/nolan.nim
+++ docs/nolan.nim
@@ -1,6 +1,17 @@
 import nimib
 import strutils, sequtils
 
+nbInit
+
+nbText: """
+> This nimib example document shows how to use `nbDoc.blocks` api to change
+> the order of presentation of blocks.
+>
+> It reorders a **Limerick** according to Nolan's [Memento film structure](https://en.wikipedia.org/wiki/Memento_(film)#Film_structure).
+>
+> Click on `Show Source` at the bottom to see the nim file that generates this document.
+"""
+
 proc mix[T](s: seq[T]): seq[T] =
   var
     i = 0
@@ -15,16 +26,19 @@ proc mix[T](s: seq[T]): seq[T] =
     inc i
     dec j
 
-nbInit
-
 nbText: "# " & mix(toSeq("Limerick")).join
 
-nbText: "There was a young man from Japan"
-nbText: "Whose limericks never would scan."
-nbText: "And when they asked why,"
-nbText: "He said \"I do try!"
-nbtext: "But when I get to the last line I try to fit in as many words as I can.\""
+nbText: "`1` There was a young man from Japan"
+nbText: "`2` Whose limericks never would scan."
+nbText: "`3` And when they asked why,"
+nbText: "`4` He said \"I do try!"
+nbtext: "`5` But when I get to the last line I try to fit in as many words as I can.\""
 
-nbDoc.blocks[1 .. 5] = mix(nbDoc.blocks[1 .. 5])
+nbDoc.blocks[2 .. 6] = mix(nbDoc.blocks[2 .. 6])
 
+nbSave
+
+# save another document without source to test the opt-out of Show Source feature
+nbDoc.context["no_source"] = true
+nbDoc.filename = nbDoc.filename.replace(".html", "_no_source.html")
 nbSave

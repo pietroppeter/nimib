d38ccc05649db8c3ba65a76d2c7f8375e27ffd0e
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-03-13 01:10:30 +0100

    add gits - git utilities

---
+++ src/nimib/gits.nim
@@ -0,0 +1,40 @@
+# git related utilities
+import std / [os, osproc, strutils]
+import nimib / paths
+
+proc isGitAvailable*: bool =
+  execCmdEx("git --version", options={}).exitcode == 0
+
+proc getGitRootDirectory*: AbsoluteDir =
+  # https://stackoverflow.com/a/957978/4178189
+  execProcess("git", args=["rev-parse", "--show-toplevel"], options={}).strip.AbsoluteDir
+
+proc getGitRemoteUrl*: string =
+  # https://stackoverflow.com/a/4089452/4178189
+  execProcess("git", args=["config", "--get", "remote.origin.url"], options={}).strip
+
+proc isOnGithub*: bool =
+  getGitRemoteUrl().startsWith("https://github.com")
+
+proc getGitRelativeUrl*(file: AbsoluteFile, branch="main"): string =
+  result = "/blob/" & branch & "/" 
+  result.add replace((file.relativeTo getGitRootDirectory()).string, "\\", "/")
+
+proc getGitRemoteUrl*(file: AbsoluteFile, branch="main"): string =
+  result = changeFileExt(getGitRemoteUrl(), "")
+  result.add getGitRelativeUrl(file, branch)
+
+when isMainModule:
+  import sugar
+  dump getGitRootDirectory()
+  dump getGitRemoteUrl()
+  assert getGitRemoteUrl() == "https://github.com/pietroppeter/nimib.git"
+
+  import nimib
+  nbInit
+  dump nbThisFile.getGitRelativeUrl
+  dump nbThisFile.getGitRemoteUrl
+  assert nbThisFile.getGitRemoteUrl == "https://github.com/pietroppeter/nimib/blob/main/src/nimib/gits.nim"
+
+  assert isGitAvailable()
+  assert isOnGithub()

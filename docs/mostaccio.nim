ac9a7b1ce5b456eda8d822f2721c318ee77d759c
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-05-06 12:27:15 +0200

    ongoing mostaccio (and apply fix to ptest)

--- docs/mostaccio.nim
+++ docs/mostaccio.nim
@@ -84,7 +84,8 @@ The Ruby version of Mustache supports raising an exception in this situation, fo
 Template:
 """ # is there a way to raise the exception in nim-mustache?
 nbCode:
-  tmpl = """* {{name}}
+  tmpl = """
+* {{name}}
 * {{age}}
 * {{company}}
 * {{{company}}}
@@ -100,15 +101,148 @@ nbCode:
 nbText: """
 > added the `{{&company}}` to clarify that it is just an alternative syntax with the same result
 > as the triple mustache.
-"""
+
 ### Sections
+
+Sections render blocks of text one or more times, depending on the value of the key in the current context.
+
+A section begins with a pound and ends with a slash.
+That is, `{{#person}}` begins a "person" section while `{{/person}}` ends it.
+
+The behavior of the section is determined by the value of the key.
+
+#### False Values or Empty Lists
+
+If the person key exists and has a value of false or an empty list, the HTML between the pound and slash will not be displayed.
+
+Template:
+"""
+nbCode:
+  tmpl = """
+Shown.
+{{#person}}
+  Never shown!
+{{/person}}"""
+nbText: "Context:"
+nbCode:
+  context = newContext()
+  context["person"] = false
+nbText: "Output:"
+nbCode:
+  echo tmpl.render(context)
+nbText: """
 #### Non-Empty Lists
+
+If the **person** key exists and has a non-false value,
+the HTML between the pound and slash will be rendered and displayed one or more times.
+
+When the value is a non-empty list, the text in the block will be displayed once for each item in the list.
+The context of the block will be set to the current item for each iteration. In this way we can loop over collections.
+
+Template:
+"""
+nbCode:
+  tmpl = """
+{{#repo}}
+  <b>{{name}}</b>
+{{/repo}}"""
+nbText: "Context:"
+nbCode:
+  import json
+  context = newContext()
+  context["repo"] = %* [
+    { "name": "resque" },
+    { "name": "hub" },
+    { "name": "rip" }
+  ]
+# add a note to explain better what is Context and what can be casted to a context value?
+nbText: "Output:"
+nbCode:
+  echo tmpl.render(context)
+nbText: """
 #### Lambdas
+
+When the value is a callable object, such as a function or lambda, the object will be invoked and passed the block of text.
+The text passed is the literal block, unrendered.
+`{{tags}}` will not have been expanded - the lambda should do that on its own. In this way you can implement filters or caching.
+
+Template:
+"""
+nbCode:
+  tmpl = """
+{{#wrapped}}
+  {{name}} is awesome.
+{{/wrapped}}"""
+nbText: "Context:"
+nbCode:
+  context = newContext()
+  context["name"] = "Willy"
+  context["wrapped"] = proc(s: string, c: Context): string = "<b>" & s.render(c) & "</b>"
+nbText: "Output:"
+nbCode:
+  echo tmpl.render(context)
+nbText: """
 #### Non-False Values
+
+When the value is non-false but not a list,
+it will be used as the context for a single rendering of the block.
+
+Template:
+"""
+nbCode:
+  tmpl = """
+{{#person?}}
+  Hi {{name}}!
+{{/person?}}"""
+nbText: "Context:"
+nbCode:
+  context = newContext()
+  context["person?"] = %* { "name": "Jon" }
+nbText: "Output:"
+nbCode:
+  echo tmpl.render(context)
+nbText: """
 ### Inverted Sections
+
+An inverted section begins with a caret (hat) and ends with a slash.
+That is `{{^person}}` begins a "person" inverted section while `{{/person}}` ends it.
+
+While sections can be used to render text one or more times based on the value of the key,
+inverted sections may render text once based on the inverse value of the key.
+That is, they will be rendered if the key doesn't exist, is false, or is an empty list.
+
+Template:
+"""
+nbCode:
+  tmpl = """
+{{#repo}}
+  <b>{{name}}</b>
+{{/repo}}
+{{^repo}}
+  No repos :(
+{{/repo}}"""
+nbText: "Context:"
+nbCode:
+  context = newContext()
+  let empty: seq[string] = @[]
+  context["repo"] = empty  # does not work with seq or array or also with %* []
+nbText: "Output:"
+nbCode:
+  echo tmpl.render(context)
+nbText: """
 ### Comments
+
+Comments begin with a bang and are ignored. The following template:
+"""
+nbCode:
+  tmpl = """<h1>Today{{! ignore me }}.</h1>"""
+nbText: "Will render as follows:"
+nbCode:
+  echo tmpl.render(newContext())
+
 ### Partials
 ### Set Delimiter
 ## COPYRIGHT
 ## SEE ALSO
 nbShow
+# idea: document that shows all spec tests of nim mustache (do this in nblog)

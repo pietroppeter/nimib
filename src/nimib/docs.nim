257d631f8130577e7984b3e498f27f9e0beb56b3
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2021-05-14 15:17:34 +0200

    create dir if it doesn't exist (#44)

--- src/nimib/docs.nim
+++ src/nimib/docs.nim
@@ -1,7 +1,11 @@
+import std/os
 import browsers
 import types
 
 proc write*(doc: NbDoc) =
+  let (dir, name, ext) = doc.filename.splitFile()
+  if not dir.dirExists:
+    createDir(dir)
   writeFile(doc.filename, doc.render(doc))
 
 proc open*(doc: NbDoc) =

bd60d6dc089fcd76a8d7b693151d0c3071fdba90
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-09-05 18:58:05 +0200

    more logging while saving

--- src/nimib/docs.nim
+++ src/nimib/docs.nim
@@ -3,9 +3,12 @@ import browsers
 import types
 
 proc write*(doc: NbDoc) =
+  echo "[nimib] current directory: ", getCurrentDir()
   let dir = doc.filename.splitFile().dir
   if not dir.dirExists:
+    echo "[nimib] creating directory: ", dir
     createDir(dir)
+  echo "[nimib] saving file: ", doc.filename
   writeFile(doc.filename, doc.render(doc))
 
 proc open*(doc: NbDoc) =

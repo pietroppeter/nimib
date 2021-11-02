f9e149cd141f80530f2eaefc3c92c912296c3488
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2021-11-02 16:03:28 +0100

    move to sources.nim and restore blocks.nim

---
+++ src/nimib/sources.nim
@@ -0,0 +1,133 @@
+import std/[macros, sugar]
+import types
+
+# Credits to @haxscramper for sharing his code on reading the line info
+# And credits to @Yardanico for making a previous attempt which I have taken much inspiration from.
+
+type
+  Pos* = object
+    line*: int
+    column*: int
+
+proc toPos(info: LineInfo): Pos =
+  Pos(line: info.line, column: info.column)
+
+proc startPos(node: NimNode): Pos =
+  ## Has column info
+  case node.kind:
+    of nnkNone .. nnkNilLit, nnkDiscardStmt, nnkCommentStmt:
+      result = toPos(node.lineInfoObj())
+
+    else:
+      result = node[0].startPos()
+
+proc finishPos(node: NimNode): Pos =
+  ## Does not have column info
+  case node.kind:
+    of nnkNone .. nnkNilLit, nnkDiscardStmt, nnkCommentStmt:
+      result = toPos(node.lineInfoObj())
+      #result.column += len($node) - 1 doesn't work for all NimNode kinds
+
+    else:
+      if len(node) > 0:
+        var idx = len(node) - 1
+        while idx >= 0 and node[idx].kind in {nnkEmpty}:
+          dec idx
+
+        if idx >= 0:
+          result = node[idx].finishPos()
+
+        else:
+          result = toPos(node.lineInfoObj())
+
+      else:
+        result = toPos(node.lineInfoObj())
+
+import std/[
+  sequtils,
+  parseutils, 
+  strutils
+  ]
+
+export
+  strutils.split, strutils.contains, strutils.strip, strutils.join, strutils.repeat, strutils.isEmptyOrWhitespace, strutils.count,
+  sequtils.mapIt,
+  parseutils.skipWhile,
+  sugar.collect
+
+proc isNbCodeLine*(s: string): bool =
+  "nbcode" in s.toLower
+
+macro nbCodeBlock*(identBlock, identContainer, body: untyped) =
+  let startPos = startPos(body)
+  let endPos = finishPos(body)
+  let lObj = body.lineInfoObj()
+  let filename = body.lineInfoObj().filename
+  #bind mapIt, skipWhile, toLower, split, contains
+  result = quote do:
+    const entireFile = staticRead(`filename`)
+    let lines = entireFile.split("\n")
+    echo "Filename: ", `lObj`
+    echo "Start line: ", `startPos`.line
+    var startLine = `startPos`.line - 1
+    var endLine = `endPos`.line - 1
+
+    var codeText: string
+    if not lines[startLine].isNbCodeLine: # multiline case
+      while 0 < startLine and not lines[startLine-1].isNbCodeLine:
+        #[ cases like this reports the third line instead of the second line:
+          nbCode:
+            let # this is the line we want
+              x = 1 # but this is the one we get
+        ]#
+        dec startLine
+
+      let indent = skipWhile(lines[startLine], {' '})
+      let indentStr = " ".repeat(indent)
+
+      if lines[endLine].count("\"\"\"") == 1: # only opening of triple quoted string found. Rest is below it. 
+        inc endLine # bump it to not trigger the loop to immediately break
+        while endLine < lines.high and "\"\"\"" notin lines[endLine]:
+          inc endLine
+          echo "Triple quote: ", lines[endLine]
+
+      while endLine < lines.high and (lines[endLine+1].startsWith(indentStr) or lines[endLine+1].isEmptyOrWhitespace):# and lines[endLine+1].strip().startsWith("#"):
+        # Ending Comments should be included as well, but they won't be included in the AST -> endLine doesn't take them into account.
+        # Block comments must be properly indented (including the content)
+        inc endLine
+
+      var codeLines = lines[startLine .. endLine]
+
+      var notIndentLines: seq[int] # these lines are not to be adjusted for indentation. Eg content of triple quoted strings.
+      var i: int
+      while i < codeLines.len:
+        if codeLines[i].count("\"\"\"") == 1:
+          # We must do the identification of triple quoted string separatly from the endLine bumping because the triple strings
+          # might not be the last expression in the code block.
+          inc i # bump it to not trigger the loop to immediately break on the initial """
+          notIndentLines.add i
+          while i < codeLines.len and "\"\"\"" notin codeLines[i]:
+            inc i
+            notIndentLines.add i
+        inc i
+        
+
+
+      let parsedLines = collect(newSeqOfCap(codeLines.len)):
+        for i in 0 .. codeLines.high:
+          if i in notIndentLines:
+            codeLines[i]
+          else:
+            codeLines[i].substr(indent)
+      codeText = parsedLines.join("\n")
+      #codeText = codeLines.mapIt(it.substr(indent)).join("\n")
+
+    else: # single line case, eg `nbCode: echo "Hello World"`
+      let line = lines[startLine]
+      codeText = line.split(":")[1 .. ^1].join(":").strip() # split at first ":" and take the rest as code and then strip it.
+
+    `identBlock` = newBlock(nbkCode, codeText.strip)
+    captureStdout(`identBlock`.output):
+      `body`
+    echoCodeBlock `identBlock`
+    `identContainer`.blocks.add `identBlock`

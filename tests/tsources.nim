5b3410b43e34760ac2666e68b5381829e4f5f2f9
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2021-11-07 18:28:05 +0100

    add basic test file

---
+++ tests/tsources.nim
@@ -0,0 +1,44 @@
+import nimib, strutils, strformat
+
+template check =
+  #echo &"---\n{nbBlock.code}\n"
+  # the replace stuff needed on windows where the lines read from file will have windows native new lines
+  if nbBlock.code.replace("\c\l", "\n").replace("\c", "\n") != expected:
+    echo &"---ERROR:\n{nbBlock.code}\n---\n{expected}"
+    echo "##################"
+
+var expected: string
+nbInit
+nbCode:
+  # a comment
+  let
+    x = 1
+# a comment here means that on windows line 12 will read with a single \c instead of \c\l (if blank space here I will have \c\l)
+expected = """
+# a comment
+let
+  x = 1
+"""
+check
+
+nbCode:
+  # a comment
+  let  # and a comment with nbCode
+    y = 1
+expected = """
+# a comment
+let  # and a comment with nbCode
+  y = 1
+"""
+check
+#[
+nbCode echo y
+expected = "echo y"
+check
+nbCode: echo y
+expected = "echo y"
+check
+nbCode(echo y)
+expected = "echo y"
+check
+]#

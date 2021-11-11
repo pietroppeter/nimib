8dfc8ba6bf7c3e9c82066908c68c7a9f10d054b1
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2021-11-11 14:56:55 +0100

    add test to CI

--- tests/tsources.nim
+++ tests/tsources.nim
@@ -1,46 +1,49 @@
 import nimib, strutils, strformat
+import unittest
 
-template check =
-  #echo &"---\n{nbBlock.code}\n"
-  # the replace stuff needed on windows where the lines read from file will have windows native new lines
-  if nbBlock.code.replace("\c\l", "\n").replace("\c", "\n") != expected:
-    echo &"---ERROR:\n{nbBlock.code}\n---\n{expected}"
-    echo "##################"
+suite "test sources":
+  template check =
+    #echo &"---\n{nbBlock.code}\n"
+    # the replace stuff needed on windows where the lines read from file will have windows native new lines
+    test $currentTest:
+      check nbBlock.code.replace("\c\l", "\n").replace("\c", "\n") == expected
+    currentTest += 1
 
-var expected: string
-nbInit
-nbCode:
-  # a comment
-  let
-    x = 1
-# a comment here means that on windows line 12 will read with a single \c instead of \c\l (if blank space here I will have \c\l)
-expected = """
+  var currentTest: int
+  var expected: string
+  nbInit
+  nbCode:
+    # a comment
+    let
+      x = 1
+  # a comment here means that on windows line 12 will read with a single \c instead of \c\l (if blank space here I will have \c\l)
+  expected = """
 # a comment
 let
   x = 1
 """
-check
+  check
 
-nbCode:
-  # a comment
-  let  # and a comment with nbCode
-    y = 1
-expected = """
+  nbCode:
+    # a comment
+    let  # and a comment with nbCode
+      y = 1
+  expected = """
 # a comment
 let  # and a comment with nbCode
   y = 1
 """
-check
+  check
 
-nbCode echo y
-expected = "echo y"
-check
-nbCode: echo y
-expected = "echo y"
-check
-nbCode(echo y)
-expected = "echo y"
-check
-nbCode ((echo ("( This is ( weird string)")))
-expected = "echo (\"( This is ( weird string)\")"
-check
+  nbCode echo y
+  expected = "echo y"
+  check
+  nbCode: echo y
+  expected = "echo y"
+  check
+  nbCode(echo y)
+  expected = "echo y"
+  check
+  nbCode ((echo ("( This is ( weird string)")))
+  expected = "echo (\"( This is ( weird string)\")"
+  check

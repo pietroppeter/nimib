087c01a6156a425d5926cf6e0b2677fececadb7f
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2022-01-09 19:18:36 +0100

    use always check

--- tests/tnimib.nim
+++ tests/tnimib.nim
@@ -6,22 +6,22 @@ nbInit # todo: add a test suite for nbInit
 suite "nbText":
   test "single line text string":
     nbText: "hi"
-    doAssert nb.blk.code == "\"hi\""
-    doAssert nb.blk.output == "hi"
+    check nb.blk.code == "\"hi\""
+    check nb.blk.output == "hi"
 
   when not defined(nimibPreviewCodeAsInSource):
     test "single line text string with strformat":
       let name = "you"
       nbText: fmt"hi {name}"
-      doAssert nb.blk.code == "fmt\"hi {name}\""
-      doAssert nb.blk.output == "hi you"
+      check nb.blk.code == "fmt\"hi {name}\""
+      check nb.blk.output == "hi you"
 
     test "multi line text string":
       nbText: """hi
 how are you?
 """
-      doAssert nb.blk.code == "\"\"\"hi\nhow are you?\n\"\"\""
-      doAssert nb.blk.output == """hi
+      check nb.blk.code == "\"\"\"hi\nhow are you?\n\"\"\""
+      check nb.blk.output == """hi
 how are you?
 """
 
@@ -32,16 +32,16 @@ how are you?
       nbText: &"""hi {name}
 how are you? {answer}
 """
-      doAssert nb.blk.code == "&\"\"\"hi {name}\nhow are you? {answer}\n\"\"\""
-      doAssert nb.blk.output == """hi you
+      check nb.blk.code == "&\"\"\"hi {name}\nhow are you? {answer}\n\"\"\""
+      check nb.blk.output == """hi you
 how are you? fine
 """
 
 suite "nbCode":
   test "single line of code, no output":
     nbCode: discard
-    doAssert nb.blk.code == "discard"
-    doAssert nb.blk.output == ""
+    check nb.blk.code == "discard"
+    check nb.blk.output == ""
 
   test "single line of code, with output":
     nbCode: echo "hi"

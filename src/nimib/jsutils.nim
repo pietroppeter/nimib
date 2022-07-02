4b9c8aec8b3930cb9c1ab81cb7a37512e4afd0dc
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2022-07-02 13:35:42 +0200

    ignore to gensym procs inside karaxHtml

--- src/nimib/jsutils.nim
+++ src/nimib/jsutils.nim
@@ -29,26 +29,27 @@ macro addInvalid(key: string, s: untyped): untyped =
   if key.strVal notin invalidCodeTable:
     invalidCodeTable[key.strVal] = s
 
-proc gensymProcIterConverter(n: NimNode) =
+proc gensymProcIterConverter(n: NimNode, replaceProcs: bool) =
   ## By default procs, iterators and converters are injected and will share the same name in the resulting javascript.
   ## Therefore we gensym them here to give them unique names. Also replace the references to it.
+  ## replaceProcs: whether to replace procs names or not. It will replace existing names regardless.
   for i in 0 ..< n.len:
     case n[i].kind
     of nnkProcDef:
-      echo "Procdef found!!!!"
-      if n[i][0].kind == nnkPostfix: # foo*
-        let oldIdent = n[i][0][1].strVal
-        let newIdent = gensym(ident=oldIdent).repr.ident
-        n[i][0][1] = newIdent
-        tabMapIdents[oldIdent] = newIdent
-      else:
-        let oldIdent = n[i][0].strVal
-        let newIdent = gensym(ident=oldIdent).repr.ident
-        n[i][0] = newIdent
-        tabMapIdents[oldIdent] = newIdent
+      if replaceProcs:
+        if n[i][0].kind == nnkPostfix: # foo*
+          let oldIdent = n[i][0][1].strVal
+          let newIdent = gensym(ident=oldIdent).repr.ident
+          n[i][0][1] = newIdent
+          tabMapIdents[oldIdent] = newIdent
+        else:
+          let oldIdent = n[i][0].strVal
+          let newIdent = gensym(ident=oldIdent).repr.ident
+          n[i][0] = newIdent
+          tabMapIdents[oldIdent] = newIdent
       # Function might be recursive or contain other procs, loop through it's body as well
       for child in n[i][6]:
-        gensymProcIterConverter(child)
+        gensymProcIterConverter(child, replaceProcs)
     of nnkIteratorDef:
       discard
     of nnkConverterDef:
@@ -66,13 +67,20 @@ proc gensymProcIterConverter(n: NimNode) =
       p[0] = newIdent
       # loop through proc body as well
       for child in p[6]:
-        gensymProcIterConverter(child)
+        gensymProcIterConverter(child, replaceProcs)
       n[i] = newStmtList(p, newIdent)
     of nnkSym, nnkIdent:
       if n[i].strVal in tabMapIdents:
         n[i] = tabMapIdents[n[i].strVal]
+    of nnkCall:
+      # Check if it is karaxHtml:
+      # if so set replaceProcs = false for the children
+      if n[i][0].eqIdent("karaxHtml"):
+        gensymProcIterConverter(n[i][1], false)
+      else:
+        gensymProcIterConverter(n[i], replaceProcs)
     else:
-      gensymProcIterConverter(n[i])
+      gensymProcIterConverter(n[i], replaceProcs)
 
 proc degensymAst(n: NimNode, removeGensym = false) =
   for i in 0 ..< n.len:
@@ -141,7 +149,7 @@ macro nimToJsStringSecondStage*(key: static string, captureVars: varargs[typed])
   let (capAssignments, placeholders) = genCapturedAssignment(captureVars, captureTypes)
   # 2. Stringify code
   let code = newStmtList(capAssignments, body).copyNimTree()
-  code.gensymProcIterConverter()
+  code.gensymProcIterConverter(replaceProcs=true)
   code.degensymAst()
   var codeText = code.toStrLit
   # 3. Generate code which does the serialization and replacement of placeholders

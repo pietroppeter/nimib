726aa1fb06e193d5140813776dabf5ede40dc6f9
Grave <pmpiga1@gmail.com>
2023-03-16 14:49:09 -0400

    create `allblocks.nim` and add `nbCodeSkip` to it

---
+++ docsrc/allblocks.nim
@@ -0,0 +1,26 @@
+import nimib
+
+nbInit
+
+nbText: """
+> This nimib document provides a brief description and example for 
+> all blocks in nimib.
+"""
+
+nbText: """
+### nbCodeSkip
+
+Similar to `nbCode`, `nbCodeSkip` is a block that displays 
+highlighted code but does not compile or run it.
+
+Example(s):
+"""
+
+nbCodeSkip: 
+  echo "Notice how there's no output?"
+  echo "The code is not compiled or executed so no output is generated!"
+
+nbCodeSkip:
+  exit() # even this won't execute!
+
+nbSave

e2525037b9467c3c61db033e33986514d55f6fcb
Dimitri Lesnoff <dimitri.lesnoff@gmail.com>
2023-03-28 15:25:35 +0200

    Allblocks: nbImage, nbTextWithCode, nbRawHtml, nbPython

--- docsrc/allblocks.nim
+++ docsrc/allblocks.nim
@@ -1,5 +1,7 @@
 import nimoji
 import nimib
+import nimpy
+import std/[math, strformat]
 
 nbInit
 
@@ -19,7 +21,7 @@ highlighted code but does not compile or run it.
 Example(s):
 """
 
-nbCodeSkip: 
+nbCodeSkip:
   echo "Notice how there's no output?"
   echo "The code is not compiled or executed so no output is generated!"
 
@@ -31,10 +33,72 @@ nbText: """
 
 `nbCapture` is a block that only shows the captured output of a code block.
 
-Example(s):
+Example:
+
+The source code is pasted here to avoid you looking it up:
+```nim
+nbCapture:
+  echo "Captured!"
+```
+And below is the actual block:
 """
 
 nbCapture:
   echo "Captured!"
+
+nbText: """
+### nbImage
+`nbImage` enables to display your favorite pictures.
+```nim
+nbImage(url="images/todd-cravens-nimib-unsplash.jpg", caption="Blue Whale (photograph by Todd Cravens)")
+```
+Most formats are accepted. The caption is optional!
+"""
+
+nbImage(url="images/todd-cravens-nimib-unsplash.jpg", caption="Blue Whale (photograph by Todd Cravens)")
+
+nbText:"""
+### nbTextWithCode
+You can run Nim code directly in your markdown text with this block. It may be of use for instance for computation inside tables. See `numerical.nim`.
+You just have to enclose your code into brackets e.g. `{Math.PI}` !.
+"""
+
+nbTextWithCode:fmt"""
+The *fifteen* first digits of pi are {math.PI}. This constant defines the *ratio* between the *diameter* and the *perimeter* of a circle.
+"""
+
+nbText:"""
+### nbRawHtml
+Certain things are not doable with pure Markdown. You can use raw HTML directly with the `nbRawHtml` block.
+
+For example, you have to use HTML style attribute and inject CSS styling in a HTML tag to center your titles (that is a Markdown limitation).
+
+Here is the source code:
+```html
+<h2 style="text-align: center">Centered title</h2>
+```
+and the centered title:
+"""
+
+nbRawHtml:"""<h2 style="text-align: center">Centered title</h2>"""
+
+nbText:"""
+### nbPython
+Python is supported too !
+There are two requirements for the `nbPython` block.
+First you need to install `nimpy` and import it in your nimib script.
+Second, you need to call `nbInitPython()`.
+"""
+
+nbInitPython()
+nbPython:"""
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+fib(1000)
+"""
 
 nbSave

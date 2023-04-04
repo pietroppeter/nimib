51f5f27a15476e4db0956bfc11e34d47d5bdeea9
dlesnoff <54949944+dlesnoff@users.noreply.github.com>
2023-04-04 10:05:46 +0200

    Update docsrc/allblocks.nim
    Co-authored-by: Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>

--- docsrc/allblocks.nim
+++ docsrc/allblocks.nim
@@ -39,7 +39,7 @@ nimibCode:
 nbCodeBlock: "nbCode"
 nbText: """
 You can insert your Nim code in your text using the `nbCode` block.
-It displays your code with highlighting, compiles and runs it.
+It displays your code with highlighting, compiles and runs it and captures any output that is `echo`'d.
 """
 
 nimibCode:

ead11e436e7a9d45da58c7bf1f48b7de7f4bb7c2
Wasin Chintapanyakul <sentbysent@gmail.com>
2023-08-10 08:01:12 +0700

    Remove filename overload

--- src/nimib/capture.nim
+++ src/nimib/capture.nim
@@ -1,20 +1,15 @@
 ## This submodule implements only one template to temporarily redirect and capture stdout during execution of a chunk of code.
 import fusion/ioutils
-import std/os
 import std/tempfiles
 
-template captureStdout*(ident: untyped, filename: string, body: untyped) =
+template captureStdout*(ident: untyped, body: untyped) =
     ## redirect stdout to a temporary file and captures output of body in ident
     # Duplicate stdout
     let stdoutFileno: FileHandle = stdout.getFileHandle()
     let stdoutDupFd: FileHandle = stdoutFileno.duplicate()
     
     # Create a new temporary file or attempt to open it
-    let (tmpFile, tmpFileName {.used.}) = if filename == "":
-            createTempFile("tmp", "")
-        else:
-            let tmpName = getTempDir() / filename
-            (tmpName.open(fmAppend), tmpName)
+    let (tmpFile, _) = createTempFile("tmp", "")
     let tmpFileFd: FileHandle = tmpFile.getFileHandle() 
     
     # writing to stdoutFileno now writes to tmpFile
@@ -26,15 +21,9 @@ template captureStdout*(ident: untyped, filename: string, body: untyped) =
     # Flush stdout and tmpFile, read tmpFile from start to ident and then close tmpFile
     stdout.flushFile()
     tmpFile.flushFile()
-    if filename == "":
-        tmpFile.setFilePos(0)
-    else:
-        discard tmpFile.reopen(tmpFileName)
+    tmpFile.setFilePos(0)
     ident = tmpFile.readAll()
     tmpFile.close()
     
     # Restore stdout
     stdoutDupFd.duplicateTo(stdoutFileno)
-
-template captureStdout*(ident: untyped, body: untyped) =
-    captureStdout(ident, "", body)
